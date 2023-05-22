a1f9ac4e107319ac0a46cb19955b9b4b53e669cf
Jessie  Detchou <jessie.detchou@gmail.com>
2023-05-22 19:34:10 -0400

    update tabs

--- src/components/BlockFour.tsx
+++ src/components/BlockFour.tsx
@@ -20,19 +20,35 @@ import {
 	VStack,
 } from '@chakra-ui/react';
 import { ScrollAnimation } from './ScrollAnimation';
-import easyToUseImage from '../public/easyToUseImage.png';
+import Target from '../public/Target.png';
+import Gauge from '../public/Gauge.png';
+import DeviceMobile from '../public/DeviceMobile.png';
+
 import Image from 'next/image';
 
 export const BlockFour = ({ title }: { title: string }) => (
-	<VStack paddingY={10}>
-		<Box width={'60%'}>
-			<Tabs variant="unstyled">
-				<TabList color={'grey'}>
-					<Tab color={'brand.100'} fontSize={{ xs: 'xs', md: 'xs' }}>
+	<VStack paddingY={10} >
+			<Tabs variant="unstyled"  >
+				<TabList color={'grey'} width={"full"} justifyContent={'center'}>
+					<Tab
+						fontSize={{ xs: 'xs', md: 'xs' }}
+						_selected={{ color: 'brand.100', borderColor: 'brand.100' }}
+						_focus={{ outline: 'none' }}
+					>
 						Easy to use
 					</Tab>
-					<Tab fontSize={{ xs: 'xs', md: 'xs' }}>Instant</Tab>
-					<Tab fontSize={{ xs: 'xs', md: 'xs' }}>Dynamic</Tab>
+					<Tab
+						fontSize={{ xs: 'xs', md: 'xs' }}
+						_selected={{ color: 'brand.100', borderColor: 'brand.100' }}
+					>
+						Instant
+					</Tab>
+					<Tab
+						fontSize={{ xs: 'xs', md: 'xs' }}
+						_selected={{ color: 'brand.100', borderColor: 'brand.100' }}
+					>
+						Accurate
+					</Tab>
 				</TabList>
 				<TabIndicator
 					mt="-1.5px"
@@ -40,64 +56,45 @@ export const BlockFour = ({ title }: { title: string }) => (
 					bg="brand.100"
 					borderRadius="1px"
 				/>
-				<TabPanels>
+				<TabPanels width={"50%"}>
 					<TabPanel>
 						<VStack>
-							<Box width={'90%'} paddingY={5}>
+							<Box paddingY={5} width={"50%"}>
 								<Text color={'white'} textAlign={'center'} fontWeight={400}>
-								An easy to use interface for investors familar with platforms such as robinhood or coinbase.
+									An easy to use interface for investors familar with platforms
+									such as robinhood or coinbase.
 								</Text>
 							</Box>
-							<Image src={easyToUseImage} style={{ borderRadius: 15 }} />
+							<Image src={DeviceMobile} style={{ borderRadius: 15 }} />
+						</VStack>
+					</TabPanel>
+
+					<TabPanel>
+						<VStack>
+							<Box  paddingY={5} width={"50%"}>
+								<Text color={'white'} textAlign={'center'} fontWeight={400}>
+									The Xchange is real-time allowing you to react to market
+									conditions in a near instance
+								</Text>
+							</Box>
+							<Image src={Gauge} style={{ borderRadius: 15 }} />
 						</VStack>
 					</TabPanel>
 					<TabPanel>
 						<VStack>
-							<Box width={'90%'} paddingY={5}>
+							<Box paddingY={5} width={"50%"}>
 								<Text color={'white'} textAlign={'center'} fontWeight={400}>
-									Xsauce allows you to predict the price of the next drop. Use
-									your knowledge of culture and earn money
+									We source our price feeds from a combination of industry
+									leading resale platforms such as StockX, Goat, and Flight Club
+									to name a few.
 								</Text>
 							</Box>
-							<Image src={easyToUseImage} style={{ borderRadius: 15 }} />
-						</VStack>
-					</TabPanel>
-					<TabPanel>
-						<VStack>
-							<Box width={'90%'} paddingY={5}>
-								<Text color={'white'} textAlign={'center'} fontWeight={400}>
-									Xsauce allows you to predict the price of the next drop. Use
-									your knowledge of culture and earn money
-								</Text>
-							</Box>
-							<Image src={easyToUseImage} style={{ borderRadius: 15 }} />
-						</VStack>
-					</TabPanel>
-					<TabPanel>
-						<VStack>
-							<Box width={'90%'} paddingY={5}>
-								<Text color={'white'} textAlign={'center'} fontWeight={400}>
-									Xsauce allows you to predict the price of the next drop. Use
-									your knowledge of culture and earn money
-								</Text>
-							</Box>
-							<Image src={easyToUseImage} style={{ borderRadius: 15 }} />
-						</VStack>
-					</TabPanel>
-					<TabPanel>
-						<VStack>
-							<Box width={'90%'} paddingY={5}>
-								<Text color={'white'} textAlign={'center'} fontWeight={400}>
-									Xsauce allows you to predict the price of the next drop. Use
-									your knowledge of culture and earn money
-								</Text>
-							</Box>
-							<Image src={easyToUseImage} style={{ borderRadius: 15 }} />
+							<Image src={Target} style={{ borderRadius: 15 }} />
 						</VStack>
 					</TabPanel>
 				</TabPanels>
 			</Tabs>
-		</Box>
+
 	</VStack>
 );
 
