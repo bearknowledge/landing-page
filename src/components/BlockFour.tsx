d18d791bf695d34ec3ae7b75759c3e85d5838bf2
Jessie  Detchou <jessie.detchou@gmail.com>
2023-05-23 16:00:54 -0400

    update svg

--- src/components/BlockFour.tsx
+++ src/components/BlockFour.tsx
@@ -30,72 +30,76 @@ import DeviceMobile from '../public/DeviceMobile.png';
 import Image from 'next/image';
 
 export const BlockFour = ({ title }: { title: string }) => (
-	<VStack paddingY={10} >
-			<Tabs variant="unstyled"  >
-				<TabList color={'grey'} width={"full"} justifyContent={'center'}>
-					<Tab
-						fontSize={{ xs: 'xs', md: 'xs' }}
-						_selected={{ color: 'brand.100', borderColor: 'brand.100' }}
-						_focus={{ outline: 'none' }}
-					>
-						Easy to use
-					</Tab>
-					<Tab
-						fontSize={{ xs: 'xs', md: 'xs' }}
-						_selected={{ color: 'brand.100', borderColor: 'brand.100' }}
-					>
-						Instant
-					</Tab>
-					<Tab
-						fontSize={{ xs: 'xs', md: 'xs' }}
-						_selected={{ color: 'brand.100', borderColor: 'brand.100' }}
-					>
-						Accurate
-					</Tab>
-				</TabList>
-				<TabIndicator
-					mt="-1.5px"
-					height="2px"
-					bg="brand.100"
-					borderRadius="1px"
-				/>
-				<TabPanels width={"50%"}>
-					<TabPanel>
-						<VStack>
-							<Box paddingY={5} width={"50%"}>
-								<Text color={'white'} textAlign={'center'} fontWeight={400}>
-									An easy to use interface for investors familar with platforms
-									such as robinhood or coinbase.
-								</Text>
-							</Box>
-							<EasyToUse/>
-						</VStack>
-					</TabPanel>
+	<VStack paddingY={10}>
+		<Tabs variant="unstyled">
+			<TabList color={'grey'} width={'full'} justifyContent={'center'}>
+				<Tab
+					fontSize={{ xs: 'xs', md: 'xs' }}
+					_selected={{ color: 'brand.100', borderColor: 'brand.100' }}
+					_focus={{ outline: 'none' }}
+				>
+					Easy to use
+				</Tab>
+				<Tab
+					fontSize={{ xs: 'xs', md: 'xs' }}
+					_selected={{ color: 'brand.100', borderColor: 'brand.100' }}
+				>
+					Instant
+				</Tab>
+				<Tab
+					fontSize={{ xs: 'xs', md: 'xs' }}
+					_selected={{ color: 'brand.100', borderColor: 'brand.100' }}
+				>
+					Accurate
+				</Tab>
+			</TabList>
+			<TabIndicator
+				mt="-1.5px"
+				height="2px"
+				bg="brand.100"
+				borderRadius="1px"
+			/>
+			<TabPanels width={'50%'}>
+				<TabPanel>
+					<VStack>
+						<Box paddingY={5} width={'50%'}>
+							<Text color={'white'} textAlign={'center'} fontWeight={400}>
+								An easy to use interface for investors familar with platforms
+								such as robinhood or coinbase.
+							</Text>
+						</Box>
+						<EasyToUse />
+					</VStack>
+				</TabPanel>
 
-					<TabPanel>
-						<VStack>
-							<Box  paddingY={5} width={"50%"}>
-								<Text color={'white'} textAlign={'center'} fontWeight={400}>
-									The Xchange is real-time allowing you to react to market
-									conditions in a near instance
-								</Text>
-							</Box>
-							<Instant/>
-						</VStack>
-					</TabPanel>
-					<TabPanel>
-						<VStack>
-							<Box paddingY={5} width={"50%"}>
-								<Text color={'white'} textAlign={'center'} fontWeight={400}>
-								We source our price feeds from a combination of industry leading resale platforms such as StockX, Goat,  and Flight Club to name a few.
-								</Text>
-							</Box>
-							<Box width={"50%"}><Accurate/></Box>
-						</VStack>
-					</TabPanel>
-				</TabPanels>
-			</Tabs>
+				<TabPanel>
+					<VStack>
+						<Box paddingY={5} width={'50%'}>
+							<Text color={'white'} textAlign={'center'} fontWeight={400}>
+								The Xchange is real-time allowing you to react to market
+								conditions in a near instance
+							</Text>
+						</Box>
+						<Instant />
+					</VStack>
+				</TabPanel>
+				<TabPanel>
+					<VStack>
+						<Box paddingY={5} width={'50%'}>
+							<Text color={'white'} textAlign={'center'} fontWeight={400}>
+								We source our price feeds from a combination of industry leading
+								resale platforms such as StockX, Goat, and Flight Club to name a
+								few.
+							</Text>
+						</Box>
 
+							<Accurate/>
+
+
+					</VStack>
+				</TabPanel>
+			</TabPanels>
+		</Tabs>
 	</VStack>
 );
 
